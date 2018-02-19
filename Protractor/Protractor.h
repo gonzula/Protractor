948b62e2e24a288ec2593217692a13895a124032
Gonzo Fialho <carloseaf@gmail.com>
2018-02-19 17:07:21 -0300

    Initial Commit

---
+++ Protractor/Protractor.h
@@ -0,0 +1,19 @@
+//
+//  Protractor.h
+//  Protractor
+//
+//  Created by Gonzo Fialho on 19/02/18.
+//  Copyright © 2018 Gonzo Fialho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Protractor.
+FOUNDATION_EXPORT double ProtractorVersionNumber;
+
+//! Project version string for Protractor.
+FOUNDATION_EXPORT const unsigned char ProtractorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Protractor/PublicHeader.h>
+
+
